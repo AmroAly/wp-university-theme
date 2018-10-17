9b8290a6429cbf4f654e58fb0fd5b736b73aa603
Amr Aly <amr.aly57@yahoo.com>
2018-10-17 11:21:29 +0200

    Added dynamic links instead of static

--- header.php
+++ header.php
@@ -4,22 +4,21 @@
     <?php wp_head(); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Fictional University</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo('site_name') ?></title>
+    
 </head>
 <body>
 <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="#"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Programs</a></li>
+            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
             <li><a href="#">Blog</a></li>
