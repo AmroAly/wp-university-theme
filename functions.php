39be9edcd54f0da47659892623095851387f2fbd
Amr Aly <amr@twgates.local>
2018-11-12 17:48:29 +0200

    Create custom post type 'events'

--- functions.php
+++ functions.php
@@ -15,4 +15,3 @@ function university_features() {
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title');
 }
-add_action('after_setup_theme', 'university_features');
