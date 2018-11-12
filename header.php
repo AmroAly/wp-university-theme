6eadd6f24aed4686bccfb0e0c8e587d96a2fae16
Amr Aly <amr.aly57@yahoo.com>
2018-11-12 04:13:06 +0200

    fix some links

--- header.php
+++ header.php
@@ -22,11 +22,11 @@
           ]);
           ?> -->
           <ul>
-            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 21) echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 21) echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/about') ?>">About Us</a></li>
             <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+            <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
